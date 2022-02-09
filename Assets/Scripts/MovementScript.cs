66f01659b427bc2700fb5bd47c3e249c43300eed
DevenMarrero <devensmarrero@gmail.com>
2022-02-08 20:24:15 -0800

    Added basic player movement

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -7,6 +7,19 @@ public class MovementScript : MonoBehaviour
 
     // DEVEN WRITE YOUR CODE HERE!!!
 
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +29,29 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // check if player is on the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // get keyboard input
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // apply movement to player
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        // apply gravity to player
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
